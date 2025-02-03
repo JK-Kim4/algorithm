5985194717c68c71fac097a71d511428ebcb7178
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-02-03 23:56:32 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 59.20 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        Stack<Integer> stack = new Stack<>();
+        
+        
+        for(int i : arr){
+            if(stack.empty()){
+                stack.push(i);
+                continue;
+            }
+            
+            if(stack.peek() != i) stack.push(i);
+        }
+        
+        answer = new int[stack.size()];
+        
+        for(int i = answer.length -1; i >= 0; i--){
+            answer[i] = stack.pop();
+        }
+        
+
+        return answer;
+    }
+}
