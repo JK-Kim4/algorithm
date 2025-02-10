5c44a757c2deb7cc89de4a9ccda11940d7b6dc2c
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-02-11 01:22:45 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -6,15 +6,9 @@ class Solution {
         int idx = 0;
         
         for(int[] intA : commands){
-            
-            if(intA[0] == intA[1]) {
-                answer[idx++] = array[intA[0]-1];
-                continue;
-            } else {
-                int[] temp = Arrays.copyOfRange(array, intA[0]-1, intA[1]);
-                Arrays.sort(temp);
-                answer[idx++] = temp[intA[2]-1];
-            }
+            int[] temp = Arrays.copyOfRange(array, intA[0]-1, intA[1]);
+            Arrays.sort(temp);
+            answer[idx++] = temp[intA[2]-1];
         }
         return answer;
     }
