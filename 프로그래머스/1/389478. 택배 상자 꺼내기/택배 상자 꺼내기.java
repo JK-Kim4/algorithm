5eb16a16d5c362a7b63d77fd2648409f27d5bfea
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-06-10 13:33:41 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int w, int num) {
+                int answer = 0;
+        while (num <= n) {
+            num += 2 * (w - ((num - 1) % w) - 1) + 1;
+            answer ++;
+        }
+        return answer;
+    }
+}
