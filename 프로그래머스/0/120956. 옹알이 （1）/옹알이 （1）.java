99caeb585d571ec1db6308208041f1f2ddf00428
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-06-15 23:27:47 +0900

    [level 0] Title: 옹알이 (1), Time: 5.27 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        
+        //replcaAll
+        for(String str : babbling){
+            //wyeoo
+            str = str.replaceAll("aya", "-").replaceAll("ye", "-")
+                .replaceAll("woo", "-").replaceAll("ma", "-");
+            
+            if("".equals(str.replaceAll("-", ""))){
+                answer++;
+            }
+            
+            
+        }
+            
+        return answer;
+    }
+}
