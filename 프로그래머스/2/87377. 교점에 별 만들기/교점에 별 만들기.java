5116f98b38989dbbdd752ee2a5cdcb685eb0b973
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-06-18 14:37:33 +0900

    [level 2] Title: 교점에 별 만들기, Time: 48.56 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,80 +1,97 @@
 import java.util.*;
 
-public class Solution {
+class Solution {
     private static class Point {
-        public final long x, y;
-
-        private Point(long x, long y) {
+        private final long x;
+        private final long y;
+        
+        private Point (long x, long y) {
             this.x = x;
             this.y = y;
-        }
+        }   
     }
-
-    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
-        double x = (double) (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
-        double y = (double) (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1);
-
-        if (x % 1 != 0 || y % 1 != 0) return null;
-
-        return new Point((long) x, (long) y);
+    
+    private Point findIntersection(long a, long b, long e, long c, long d, long f) {
+        double x = (double) (b*f - e*d) / (a*d - b*c);
+        double y = (double) (e*c - a*f) / (a*d - b*c);
+        
+        if ( x % 1 != 0 || y % 1 != 0) return null;
+        
+        return new Point((long)x, (long)y);
     }
-
+    
     private Point getMinimumPoint(List<Point> points) {
-        long x = Long.MAX_VALUE;
-        long y = Long.MAX_VALUE;
-
+        long minX = Long.MAX_VALUE;
+        long minY = Long.MAX_VALUE;
+        
         for (Point p : points) {
-            if (p.x < x) x = p.x;
-            if (p.y < y) y = p.y;
+            if (minX > p.x) minX = p.x;
+            if (minY > p.y) minY = p.y;
         }
-
-        return new Point(x, y);
+        
+        return new Point(minX, minY);
+        
     }
-
+    
     private Point getMaximumPoint(List<Point> points) {
-        long x = Long.MIN_VALUE;
-        long y = Long.MIN_VALUE;
-
+         long maxX = Long.MIN_VALUE;
+         long maxY = Long.MIN_VALUE;
+        
         for (Point p : points) {
-            if (p.x > x) x = p.x;
-            if (p.y > y) y = p.y;
+            if (maxX < p.x) maxX = p.x;
+            if (maxY < p.y) maxY = p.y;
         }
-
-        return new Point(x, y);
+        
+        return new Point(maxX, maxY);
     }
-
+    
+    
+    
     public String[] solution(int[][] line) {
-        List<Point> points = new ArrayList<>();
-        for (int i = 0; i < line.length; i++) {
-            for (int j = i + 1; j < line.length; j++) {
-                Point intersection = intersection(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
-                if (intersection != null) {
-                    points.add(intersection);
-                }
+        String[] answer = {};
+        
+        //교점을 담을 변수
+        List<Point> intersections = new ArrayList<>();
+        
+        //lines 순회하며 정수로된 교점 추출
+        for (int i = 0; i < line.length-1; i++) {
+            for (int j = i+1; j < line.length; j++) {
+                //정수 여부 검증
+                Point point = findIntersection(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
+                
+                if (point != null) intersections.add(point);
+            } 
+        }
+        
+        //최소 최대 좌표 구하기
+        Point minPoint = getMinimumPoint(intersections);
+        Point maxPoint = getMaximumPoint(intersections);
+        
+        int width = (int) (maxPoint.x - minPoint.x + 1);
+        int height = (int) (maxPoint.y - minPoint.y + 1);
+        
+        System.out.println("W = " + width + ", H = " + height);
+        
+        //grid 초기화
+        char[][] grid = new char[height][width];
+        for (char[] c : grid) {
+            for (int i = 0; i < c.length; i++) {
+                c[i] = '.';
             }
         }
-
-        Point minimum = getMinimumPoint(points);
-        Point maximum = getMaximumPoint(points);
-
-        int width = (int) (maximum.x - minimum.x + 1);
-        int height = (int) (maximum.y - minimum.y + 1);
-
-        char[][] arr = new char[height][width];
-        for (char[] row : arr) {
-            Arrays.fill(row, '.');
+        
+        for (Point intersection : intersections) {
+            int y = (int) (maxPoint.y - intersection.y);
+            int x = (int)(intersection.x - minPoint.x); 
+            grid[y][x] = '*';
+        }
+        
+        answer = new String[grid.length];
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = new String(grid[i]);
         }
 
-        for (Point p : points) {
-            int x = (int) (p.x - minimum.x);
-            int y = (int) (maximum.y - p.y);
-            arr[y][x] = '*';
-        }
-
-        String[] result = new String[arr.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = new String(arr[i]);
-        }
-        return result;
+        return answer;
     }
 }
