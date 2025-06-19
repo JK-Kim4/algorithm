be670dd8abd4f1c4ea78cd16e019a6e9d77e3982
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-06-19 11:12:58 +0900

    [level 2] Title: 삼각 달팽이, Time: 27.92 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,37 +1,58 @@
 class Solution {
-    
-    private static final int[] dx = {0, 1, -1};
-    private static final int[] dy = {1, 0, -1};
-    
     public int[] solution(int n) {
-         int[][] triangle = new int[n][n];
-        int v = 1;
+        //1.전달받은 파라미터로 n x n 2차원 배열 생성
+        int[][] answer = new int[n][n];        
         int x = 0;
         int y = 0;
-        int d = 0;
-
+        int value = 1; 
+        
+        //2. 값 입력 반복
         while (true) {
-            triangle[y][x] = v++;
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
-                d = (d + 1) % 3;
-                nx = x + dx[d];
-                ny = y + dy[d];
-                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
+            
+            //2.A 아래(y+1)로 이동
+            while (true) {
+                answer[y][x] = value++;
+                if (y + 1 == n || answer[y+1][x] != 0) break;
+                y++;
             }
-            x = nx;
-            y = ny;
+            
+            if (x + 1 == n || answer[y][x+1] != 0) break;
+            x++;
+            
+            //2.B 오른쪽(x+1)로 이동
+            while (true) {
+                answer[y][x] = value++;
+                if (x + 1 == n || answer[y][x+1] != 0) break;
+                x++;
+            }
+            
+            if (y-1 < 0 || x-1 < 0 || answer[y-1][x-1] != 0) break;
+            x--;
+            y--;
+            
+            //2.C 좌상(x-1, y-1)으로 이동
+            while (true) {
+                answer[y][x] = value++;
+                
+                if (y-1 < 0 || x-1 < 0 || answer[y-1][x-1] != 0) break;
+                x--;
+                y--;
+            }
+            
+            if (y < 0 || y > n - 1 || answer[y + 1][x] != 0) break;
+            y++;
+            
         }
-
-        int[] result = new int[v - 1];
+        
+        int[] result = new int[value-1];
         int index = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                result[index++] = triangle[i][j];
+            for (int j = 0; j < i+1; j++) {
+                result[index++] = answer[i][j];
             }
         }
-
+        
+        
         return result;
     }
 }
