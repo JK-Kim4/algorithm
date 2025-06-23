861fedacda1d12f0dc96e65019216423f48d8de4
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-06-23 14:33:40 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,54 @@
+class Solution {
+    
+    public String convert(String s) {
+        if(s.contains("zero")) {
+            s = s.replace("zero", "0");
+        }
+        
+        if(s.contains("one")) {
+            s = s.replace("one", "1");
+        }
+        
+        if(s.contains("two")) {
+            s = s.replace("two", "2");
+        }
+        
+        if(s.contains("three")) {
+            s = s.replace("three", "3");
+        }
+        
+        if(s.contains("four")) {
+            s = s.replace("four", "4");
+        }
+        
+        if(s.contains("five")) {
+            s = s.replace("five", "5");
+        }
+        
+        if(s.contains("six")) {
+            s = s.replace("six", "6");
+        }
+        
+        if(s.contains("seven")) {
+            s = s.replace("seven", "7");
+        }
+        
+        if(s.contains("eight")) {
+            s = s.replace("eight", "8");
+        }
+        
+        if(s.contains("nine")) {
+            s = s.replace("nine", "9");
+        }
+        
+        return s;
+    }
+    
+    
+    
+    public int solution(String s) {    
+        s = convert(s);
+        
+        return Integer.parseInt(s);
+    }
+}
