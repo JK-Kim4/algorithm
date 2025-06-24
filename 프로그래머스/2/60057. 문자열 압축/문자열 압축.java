cdf86bb14002a72af4d1c8104458cc97f9e9a438
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-06-24 15:53:33 +0900

    [level 2] Title: 문자열 압축, Time: 9.28 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -2,48 +2,48 @@ import java.util.*;
 
 class Solution {
     
-    private int compress(String source, int length) {
+    public String compress(List<String> units) {
         StringBuilder sb = new StringBuilder();
         
         String last = "";
         int count = 0;
-        for (String token : split(source, length)) {
-            if (token.equals(last)) {
+        for (String unit : units) {
+            if (unit.equals(last)) {
                 count++;
             } else {
                 if (count > 1) sb.append(count);
                 sb.append(last);
-                last = token;
+                last = unit;
                 count = 1;
             }
         }
-        
         if (count > 1) sb.append(count);
         sb.append(last);
         
-        return sb.length();
+        return sb.toString();
     }
     
-    public List<String> split(String source, int length) {
-        List<String> tokens = new ArrayList<>();
-        for (int startIndex = 0; startIndex < source.length(); startIndex += length) {
-            int endIndex = startIndex + length;
-            if (endIndex > source.length()) endIndex = source.length();
-            tokens.add(source.substring(startIndex, endIndex));
-        }
+    public List<String> split(String s, int length) {
+        List<String> result = new ArrayList<>();
         
-        return tokens;
+        for (int startIndex = 0; startIndex < s.length(); startIndex += length) {
+            int endIndex = startIndex + length;
+            if (endIndex >= s.length()) endIndex = s.length();
+            result.add(s.substring(startIndex, endIndex));
+        }
+        return result;
     }
-    
     
     public int solution(String s) {
+        if (s.length() == 1) return 1;
+        
         int min = Integer.MAX_VALUE;
-        for (int length = 1; length <= s.length(); length++) {
-            int compressed = compress(s, length);
-            if (compressed < min) {
-                min = compressed;
-            }
+        
+        for (int length = 1; length < s.length(); length++) {
+            String compressed = compress(split(s, length));
+            if (compressed.length() < min) min = compressed.length();
         }
+        
         return min;
     }
 }
