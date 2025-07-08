1dac3b7197b27e8f9e92fbc45d1b5f4858b51ab6
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-07-08 17:27:07 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 1.35 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     
+    //연산자 우선순위 정적 변수 선언
     private static final String[][] precedences = {
         "+-*".split(""),
         "+*-".split(""),
@@ -11,8 +12,9 @@ class Solution {
         "*+-".split(""),  
     };
     
-    private long calculate(String oprator, long x, long y) {
-        return switch(oprator) {
+    //연산 결과 
+    public long calculate(String operation, long x, long y) {
+        return switch(operation) {
             case "+" -> x + y;
             case "-" -> x - y;
             case "*" -> x * y;
@@ -20,41 +22,44 @@ class Solution {
         };
     }
     
-    private long calculate(List<String> tokens, String[] precedence) {
-        for (String op : precedence) {
+    public long calculate(List<String> tokens, String[] oprs) {
+        
+        for (String op : oprs) {
             for (int i = 0; i < tokens.size(); i++) {
-                if (tokens.get(i).equals(op)) {
+                if (op.equals(tokens.get(i))) {
                     long x = Long.parseLong(tokens.get(i-1));
                     long y = Long.parseLong(tokens.get(i+1));
+                    
                     long result = calculate(op, x, y);
-                    tokens.remove(i - 1);
-                    tokens.remove(i - 1);
-                    tokens.remove(i - 1);
-                    tokens.add(i - 1, String.valueOf(result));
+                    tokens.remove(i-1);
+                    tokens.remove(i-1);
+                    tokens.remove(i-1);
+                    tokens.add(i-1, String.valueOf(result));
                     i -= 2;
                 }
             }
         }
+        
         return Long.parseLong(tokens.get(0));
     }
-
+    
     
     public long solution(String expression) {
-        long answer = 0;
+        long max = 0;
         
         StringTokenizer st = new StringTokenizer(expression, "+-*", true);
         List<String> tokens = new ArrayList();
-    
-        //List 형식으로 저장
+        
         while(st.hasMoreTokens()) {
             tokens.add(st.nextToken());
         }
         
-        //우선순위 완전 탐색 최대값 반환
-        for (String[] ops : precedences) {
-            long value = Math.abs(calculate(new ArrayList<>(tokens), ops));
-            if (value > answer) answer = value;
+        //ArrayList는 가변 객체기 때문에 함수 파라미터 전달 시 새로운 List 생성하여 전달
+        for (String[] oprs : precedences) {
+            long result = Math.abs(calculate(new ArrayList(tokens), oprs));
+            if (result > max) max = result;
         }
-        return answer;
+        
+        return max;
     }
 }
