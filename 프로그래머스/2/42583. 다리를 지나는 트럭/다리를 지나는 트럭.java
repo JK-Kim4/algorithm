e230ec5b776d6a6ea0a3f7721aa47af8809ed853
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-12-19 22:07:44 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 70.32 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Truck> waitT = new LinkedList<>();
+        Queue<Truck> moveT = new LinkedList<>();
+        
+        for(int i : truck_weights) {
+            waitT.offer(new Truck(i));
+        }
+        
+        int answer = 0;
+        int curWeight = 0;
+        
+        while (!waitT.isEmpty() || !moveT.isEmpty()) {
+            answer++;
+            
+            // 1. 대기중인 트럭 이동
+            if (moveT.isEmpty()) {
+                Truck t = waitT.poll();
+                curWeight += t.weight;
+                moveT.offer(t);
+                continue;
+            }
+            
+            // 2. 이동중인 트럭 이동
+            for (Truck t : moveT) {
+                t.moving();
+            }
+            
+            if (moveT.peek().position > bridge_length) {
+                Truck t = moveT.poll();
+                curWeight -= t.weight;
+            }
+            
+            if (!waitT.isEmpty() && waitT.peek().weight + curWeight <= weight ) {
+                Truck t = waitT.poll();
+                curWeight += t.weight;
+                moveT.offer(t);
+            }
+        
+        }
+        
+        return answer;
+    }
+}
+
+class Truck {
+    
+    int weight;
+    int position;
+    
+    public Truck (int weight) {
+        this.weight = weight;
+        this.position = 1;
+    }
+    
+    public void moving() {
+        this.position += 1;
+    }
+}
