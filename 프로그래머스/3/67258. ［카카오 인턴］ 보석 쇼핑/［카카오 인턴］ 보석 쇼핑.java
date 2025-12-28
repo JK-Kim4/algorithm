24e2d4e32d0915789816a4888779b353a73632e6
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-12-28 18:33:06 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 68.75 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,44 +2,46 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        int[] answer = {};
+        int n = gems.length;
+
+        // 전체 보석 종류 수
+        Set<String> gemSet = new HashSet<>(Arrays.asList(gems));
+        int kinds = gemSet.size();
+
         int start = 0;
-        int end = gems.length-1;
-        
-        Set<String> gemSet = new HashSet<>(List.of(gems));
-        
+        int end = n - 1;
+
         int startPointer = 0;
-        int endPointer = startPointer;
-        Map<String, Integer> includes = new HashMap();
+        int endPointer = 0;
+
+        Map<String, Integer> includes = new HashMap<>();
         includes.put(gems[0], 1);
-        
-        
-        while (startPointer < gems.length-1) {
-            if (includes.keySet().size() == gemSet.size()) {
-                if (endPointer - startPointer < end - start) {
+
+        while (true) {
+            // 모든 종류를 포함하고 있다면: 가능한 한 왼쪽을 줄이면서 최솟값 갱신
+            if (includes.size() == kinds) {
+                if ((end - start) > (endPointer - startPointer)) {
                     start = startPointer;
                     end = endPointer;
                 }
-                
-                includes.put(gems[startPointer], includes.get(gems[startPointer]) - 1);
-                if (includes.get(gems[startPointer]) == 0) {
-                    includes.remove(gems[startPointer]);
-                }
-                
+
+                // 윈도우 왼쪽 줄이기
+                String leftGem = gems[startPointer];
+                int cnt = includes.get(leftGem) - 1;
+                if (cnt == 0) includes.remove(leftGem);
+                else includes.put(leftGem, cnt);
+
                 startPointer++;
-            } else if (endPointer < gems.length -1 ) {
-                
-                endPointer++;
-                includes.put(gems[endPointer], includes.getOrDefault(gems[endPointer], 0) + 1);
-                
             } else {
-                break;
+                // 아직 모든 종류가 없다면: 오른쪽 확장
+                endPointer++;
+                if (endPointer == n) break; // 더 이상 확장 불가
+
+                includes.put(gems[endPointer], includes.getOrDefault(gems[endPointer], 0) + 1);
             }
-            
         }
-        
-        
-        
+
+        // 문제는 1-based index 반환
         return new int[]{start + 1, end + 1};
     }
 }
