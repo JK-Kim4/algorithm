13d9c44d6262c1831a3e63b02b554c3beeb3e1af
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2025-12-28 22:09:35 +0900

    [level 2] Title: 주식가격, Time: 53.05 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,16 +4,23 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
+        Stack<Integer> stack = new Stack();
+        
         for (int i = 0; i < prices.length; i++) {
-            int count = 0;
-            for (int j = i + 1; j < prices.length; j++) {
-                count++;
-                if (prices[i] > prices[j]) {
-                    break;
-                }
+            // 가격이 감소되는 시점에 시간 계산
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int index = stack.pop();
+                answer[index] = i - index;
             }
-            answer[i] = count;
+            
+            stack.push(i);
         }
+        
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            answer[index] = prices.length - index -1;
+        }
+        
         
         return answer;
     }
