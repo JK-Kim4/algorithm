f7b3672ec5409e591ab01624f8ca081fd80f150c
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2026-01-16 03:21:21 +0900

    [level 2] Title: 모음 사전, Time: 17.95 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,24 +2,23 @@ import java.util.*;
 
 class Solution {
     private char[] vowels = new char[] {'A','E','I','O','U'};
+
+    public int solution(String word) {
+        List<String> dictionary = generate("");
+        
+        return dictionary.indexOf(word);
+    }
     
-    private List<String> generateWords(String s) {
+    private List<String> generate(String s) {
         List<String> words = new ArrayList<>();
         words.add(s);
         
         if (s.length() == 5) return words;
         
-        for (char c : vowels) {
-            words.addAll(generateWords(s + c));
+        for (char v : vowels) {
+            words.addAll(generate(s + v));
         }
         
         return words;
-    }
-    
-    
-    public int solution(String word) {
-        List<String> dictionary = generateWords("");
-        
-        return dictionary.indexOf(word);
     }
 }
