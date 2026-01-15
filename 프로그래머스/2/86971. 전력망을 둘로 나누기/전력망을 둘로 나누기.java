73ff3ed5aacfcbee80f221f3438b25d10f4e7067
KIM Jongwan <68538676+JK-Kim4@users.noreply.github.com>
2026-01-16 03:26:09 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.07 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,11 +2,9 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] wires) {
-        List<Integer>[] adj = new ArrayList[n+1];
-        
+        List<Integer>[] adj = new ArrayList[n + 1];
         int answer = Integer.MAX_VALUE;
         
-        // parameters set
         for (int i = 1; i <= n; i++) {
             adj[i] = new ArrayList();
         }
@@ -16,37 +14,30 @@ class Solution {
             adj[wire[1]].add(wire[0]);
         }
         
-        // dfs
         for (int[] wire : wires) {
-            boolean[] visited = new boolean[n+1];
-            // wire 끊은 상태로 순회하며 count 
+            boolean[] visited = new boolean[n + 1];
             int count = dfs(wire[0], wire[0], wire[1], visited, adj);
             int diff = Math.abs(count - (n - count));
-            
             answer = Math.min(diff, answer);
         }
-        
+            
+
         return answer;
     }
+    
     
     private int dfs(int cur, int cutA, int cutB, boolean[] visited, List<Integer>[] adj) {
         int count = 1;
         visited[cur] = true;
         
-        // cur에 연결되어있는 좌표 중 방문하지 않은 노드를 찾아 재귀호출
-        for (int i : adj[cur]) {
-            if ((cur == cutA && i == cutB) || (cur == cutB && i == cutA)) continue;
+        for (int next : adj[cur]) {
+            if ((cur == cutA && next == cutB) || (cur == cutB && next == cutA)) continue;
             
-            
-            if (!visited[i]) {
-                count += dfs(i, cutA, cutB, visited, adj);
+            if (!visited[next]) {
+                count += dfs(next, cutA, cutB, visited, adj);
             }
         }
-        
+
         return count;
     }
-    
-    
-
-
 }
